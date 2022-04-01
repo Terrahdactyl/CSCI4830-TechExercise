2ed7f3bf40153d5837550659d7d6e8e9a6ac3988
terrahQ <85520440+terrahQ@users.noreply.github.com>
2022-04-01 04:05:30 -0500

    attempt at fixing table

--- webproject-TechExercise-Quinlan/src/datamodel/Team.java
+++ webproject-TechExercise-Quinlan/src/datamodel/Team.java
@@ -15,7 +15,7 @@ public class Team implements Comparable<Team> {
    @Column(name = "id") // specify the column name. Without it, it will use method name
    private Integer id;
 
-   @Column(name = "teamName")
+   @Column(name = "teamname")
    private String name;
 
    @Column(name = "owner")
